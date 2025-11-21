421284cc52b806e2b5ec314d50fe97760e2efb5a
wentzelo <oscar.g.wentzel@gmail.com>
2025-11-21 13:39:21 +0200

    Part 2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,17 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = [Player(player_dict) for player_dict in response]
+
+    print("Players from FIN:\n")
+
+    for player in players:
+        if player.nationality == "FIN":
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    def __init__(self, data: dict):
+        self.name = data.get("name")
+        self.team = data.get("team")
+        self.goals = data.get("goals", 0)
+        self.assists = data.get("assists", 0)
+        self.nationality = data.get("nationality")
+
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
