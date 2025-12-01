5ed4fc5d14f4641c98e01494f427a5dc30dde1f1
wentzelo <oscar.g.wentzel@gmail.com>
2025-12-01 16:14:05 +0200

    Weekk 5 6

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,51 @@
 class TennisGame:
+    SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
+
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_points = 0
+        self.player2_points = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_points += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_points += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self._is_tie():
+            return self._tie_score()
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        if self._has_advantage_or_win():
+            return self._advantage_or_win_score()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        return self._regular_score()
+
+    def _is_tie(self):
+        return self.player1_points == self.player2_points
+
+    def _tie_score(self):
+        if self.player1_points < 3:
+            return f"{self.SCORE_NAMES[self.player1_points]}-All"
+        return "Deuce"
+
+    def _has_advantage_or_win(self):
+        return self.player1_points >= 4 or self.player2_points >= 4
+
+    def _advantage_or_win_score(self):
+        diff = self.player1_points - self.player2_points
+
+        if diff == 1:
+            return f"Advantage {self.player1_name}"
+        elif diff == -1:
+            return f"Advantage {self.player2_name}"
+        elif diff >= 2:
+            return f"Win for {self.player1_name}"
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return f"Win for {self.player2_name}"
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+    def _regular_score(self):
+        p1 = self.SCORE_NAMES[self.player1_points]
+        p2 = self.SCORE_NAMES[self.player2_points]
+        return f"{p1}-{p2}"
