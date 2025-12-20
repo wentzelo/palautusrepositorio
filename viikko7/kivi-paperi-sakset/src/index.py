b8eae60d50621e0657bbaeb826932388b3c0fec1
wentzelo <oscar.g.wentzel@gmail.com>
2025-12-20 16:28:48 +0200

    Week 7 exercises 3&4

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,6 +1,4 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
+from peli_tehdas import luo_peli
 
 
 def main():
@@ -13,28 +11,14 @@ def main():
               )
 
         vastaus = input()
-
-        if vastaus.endswith("a"):
+        
+        peli = luo_peli(vastaus)
+        
+        if peli:
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+            peli.pelaa()
         else:
             break
 
---
+++ viikko7/kivi-paperi-sakset/src/peli_tehdas.py
@@ -0,0 +1,14 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+
+def luo_peli(tyyppi):
+    if tyyppi == "a":
+        return KPSPelaajaVsPelaaja()
+    elif tyyppi == "b":
+        return KPSTekoaly()
+    elif tyyppi == "c":
+        return KPSParempiTekoaly()
+    
+    return None
